57f29ff584fb47aaa664dd202b3ab8883295809d
ahmadslomm <156264175+ahmadslomm@users.noreply.github.com>
2026-05-25 11:34:46 +0200

    Add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+void main() { runApp(const MyApp()); }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Voicetest', debugShowCheckedModeBanner: false, home: const LoginScreen());
+  }
+}
